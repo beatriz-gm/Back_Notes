9fc87d336450c9406307a101cb6824b4d1fbb0f1
Beatriz <beatrizgalvaomedina@gmail.com>
2024-09-07 15:03:04 -0300

    Add files via upload

---
+++ src/routes/users.routes.js
@@ -0,0 +1,18 @@
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
+const upload = multer(uploadConfig.MULTER);
+
+const  { Router } = require("express");
+const usersRoutes = Router();
+
+const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
+
+module.exports = usersRoutes;
